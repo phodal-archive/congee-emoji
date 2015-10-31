54a069c4e6d6ef41204ecb75c601d0d57da1533f
Fengda HUANG <h@phodal.com>
2015-10-31 09:43:43 +0800

    rename files

---
+++ rename_files.py
@@ -0,0 +1,12 @@
+import re, glob, os
+
+def renamer(files, pattern, replacement):
+    for pathname in glob.glob(files):
+        basename= os.path.basename(pathname)
+        new_filename= re.sub(pattern, replacement, basename)
+        if new_filename != basename:
+            os.rename(
+              pathname,
+              os.path.join(os.path.dirname(pathname), new_filename))
+
+renamer("images/*.png.gif", r"^(.*)\.png.gif$", r"\1.gif")
